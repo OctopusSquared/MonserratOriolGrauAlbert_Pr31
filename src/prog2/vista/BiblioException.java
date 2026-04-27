c5a4bd90fac283cc6975596e5d252260ee3f188d
OctopusSquared <oriolmon@gmail.com>
2026-04-27 17:45:28 +0200

    BiblioException.java creada

---
+++ src/prog2/vista/BiblioException.java
@@ -0,0 +1,9 @@
+package prog2.vista;
+
+/**
+ *
+ * @author Oriol Monserrat i Albert Grau.
+ */
+public class BiblioException extends Exception {
+    public BiblioException(String message) { super(message); }
+}
