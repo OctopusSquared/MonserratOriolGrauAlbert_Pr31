07cd8f59b2c40dbe1d1b77b793d6ed9c4268e127
OctopusSquared <oriolmon@gmail.com>
2026-04-27 17:54:26 +0200

    Prestec.java i classes filles. Falta la gestió d'excepcions

---
+++ src/prog2/model/Prestec.java
@@ -0,0 +1,22 @@
+package prog2.model;
+
+public abstract class Prestec implements InPrestec {
+    private Exemplar exemplar;
+    private Usuari usuari;
+
+    public Prestec(Exemplar exemplar, Usuari usuari) {
+        this.exemplar = exemplar;
+        this.usuari = usuari;
+    }
+
+    public void setExemplar(Exemplar exemplar) { this.exemplar = exemplar; }
+    public Exemplar getExemplar() { return exemplar; }
+    public void setUsuari(Usuari usuari) { this.usuari = usuari; }
+    public Usuari getUsuari() { return usuari; }
+
+    @Override
+    public String toString() {
+        return "Exemplar=" + exemplar.getTitol() +
+                ", Usuari=" + usuari.getNom();
+    }
+}
---
+++ src/prog2/model/PrestecNormal.java
@@ -0,0 +1,55 @@
+package prog2.model;
+
+import java.util.Date;
+import prog2.vista.BiblioException;
+
+public class PrestecNormal extends Prestec {
+    private static final long DURADA = 70000; // Temps en ms
+    private Date dataCreacio;
+    private Date dataLimitRetorn;
+    private boolean retornat;
+
+    public PrestecNormal(Exemplar exemplar, Usuari usuari, Date data) {
+        super(exemplar, usuari);
+        dataCreacio = data;
+        dataLimitRetorn = new Date(data.getTime() + DURADA);
+        retornat = false;
+    }
+
+    public void setDataCreacio(Date data) { dataCreacio = data; }
+    public Date getDataCreacio() { return dataCreacio; }
+    public void setDataLimitRetorn(Date data) { dataLimitRetorn = data; }
+    public Date getDataLimitRetorn() { return dataLimitRetorn; }
+    public String tipusPrestec() { return "Normal"; }
+    public void setRetornat(boolean retornat) { this.retornat = retornat; }
+    public boolean getRetornat() { return retornat; }
+
+    /**
+     * Retornar prestec. Llança excepció si el prestec ja es vaig retornar
+     */
+    public void retorna() throws BiblioException {
+        if (retornat) throw new BiblioException("PrestecNormal.retorna(): Llibre ja retornat!");
+        else retornat = true;
+    }
+
+    /**
+     * Retornar durada prestec. La durada del prestec depen del tipus de prestec
+     */
+    public long duradaPrestec() { return DURADA; }
+
+    /**
+     * Retornar true si el prestec està endarrerit per a la data actual
+     */
+    public boolean prestecEndarrerit() {
+        return dataLimitRetorn.after(dataCreacio);
+    }
+
+    @Override
+    public String toString() {
+        return "Tipus=" + tipusPrestec() +
+                ", " + super.toString() +
+                ", Data de creacio=" + dataCreacio.toString() +
+                ", Data límit retorn" + dataLimitRetorn.toString() +
+                ", Retornat=" + (retornat?"true":"false");
+    }
+}
